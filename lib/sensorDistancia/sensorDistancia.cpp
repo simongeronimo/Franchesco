6a02a3d367e653628d6262048489c4196d9a2f72
sradium <sduran@correo.unimet.edu.ve>
2020-03-05 07:52:28 -0500

    cambio a servo

--- lib/sensorDistancia/sensorDistancia.cpp
+++ lib/sensorDistancia/sensorDistancia.cpp
@@ -1,16 +1,22 @@
 #include "sensorDistancia.h"
 #include <Stepper.h>
+#include <Servo.h>
 
 #define STEPS 360 //No se si es un numero valido
+#define INI 115
+#define DER 175
+#define IZQ 55
 
 //Pines arduino
-Stepper stepper(STEPS, 2, 3, 4, 5);
+//Stepper stepper(STEPS, 2, 3, 4, 5);
+Servo servo;
 
 void sensorDist::init_sensorDist(void)
 {
     pinMode(trig, OUTPUT);
     pinMode(echo, INPUT);
-    stepper.setSpeed(5); 
+//    stepper.setSpeed(5);
+    servo.attach(5); 
 }
 
 int sensorDist::distancia(void)
@@ -25,20 +31,24 @@ int sensorDist::distancia(void)
 
 int sensorDist::mirarDer(void)
 {
-    stepper.step(90);
+//    stepper.step(90);
+    servo.write(DER);
     delay(500);
     int distance = distancia();
     delay(100);
-    stepper.step(-90);
+//    stepper.step(-90);
+    servo.write(INI);
     return distance;
 }
 
 int sensorDist::mirarIzq(void)
 {
-    stepper.step(90);
+    //stepper.step(90);
+    servo.write(IZQ);
     delay(500);
     int distance = distancia();
     delay(100);
-    stepper.step(-90);
+    //stepper.step(-90);
+    servo.write(INI);
     return distance;
 }
